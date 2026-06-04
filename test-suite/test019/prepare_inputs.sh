4a65921adffc13f543cb8bcce85573db40456bed
Victor Yu <victor.wz.yu.1012@gmail.com>
2026-06-03 19:50:42 -0500

    Test Wannier localization in TDDFT and BSE

--- test-suite/test019/prepare_inputs.sh
+++ test-suite/test019/prepare_inputs.sh
@@ -48,9 +48,9 @@ C             0.6206777059        0.1271982848        0.6248928293
 C             0.6167428815        0.6167447445        0.1308940454
 K_POINTS gamma
 CELL_PARAMETERS angstrom
--0.000000 3.566790 3.566790
- 3.566790 0.000000 3.566790
- 3.566790 3.566790 0.000000
+0.000000 3.566790 3.566790
+3.566790 0.000000 3.566790
+3.566790 3.566790 0.000000
 EOF
 
 
@@ -63,6 +63,8 @@ input_west:
 wbse_init_control:
   wbse_init_calculation: S
   solver: TDDFT
+  localization: W
+  overlap_thr: 0.0005
 EOF
 
 
@@ -75,6 +77,8 @@ input_west:
 wbse_init_control:
   wbse_init_calculation: S
   solver: TDDFT
+  localization: W
+  overlap_thr: 0.0005
 
 wbse_control:
   wbse_calculation: D
